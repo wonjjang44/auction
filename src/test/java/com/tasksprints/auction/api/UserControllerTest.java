89210418851eb5e9b2720ac4da1d2704598370a4
na0th <105775683+na0th@users.noreply.github.com>
2024-11-01 00:48:30 +0900

    test : Wallet 생성을 User 쪽에서 하면서 테스트 코드 변경
    변경한 내용 :
    1. wallet을 User쪽에서 한 번에 생성하면서 테스트 코드를 조금 변경함
    2. UserDetailsResponse에 walletId 필드를 추가해서 테스트코드의 생성자의 파라미터로 walletId 필드 추가해줌

--- src/test/java/com/tasksprints/auction/api/UserControllerTest.java
+++ src/test/java/com/tasksprints/auction/api/UserControllerTest.java
@@ -48,7 +48,7 @@ class UserControllerTest {
         @Test
         @DisplayName("POST /api/v1/user - 성공")
         void registerUser() throws Exception {
-            UserDetailResponse userDetailResponse = new UserDetailResponse(1L, "John", "john@example.com", "password", "john123");
+            UserDetailResponse userDetailResponse = new UserDetailResponse(1L, "John", "john@example.com", "password", "john123", "1L");
 
             Mockito.when(userService.createUser(any(UserRequest.Register.class))).thenReturn(userDetailResponse);
 
@@ -67,7 +67,7 @@ class UserControllerTest {
         @Test
         @DisplayName("GET /api/v1/user/{id} - 성공")
         void getUserById() throws Exception {
-            UserDetailResponse userDetailResponse = new UserDetailResponse(1L, "John", "john@example.com", "password", "john123");
+            UserDetailResponse userDetailResponse = new UserDetailResponse(1L, "John", "john@example.com", "password", "john123", "1L");
 
             Mockito.when(userService.getUserDetailsById(anyLong())).thenReturn(userDetailResponse);
 
@@ -97,7 +97,7 @@ class UserControllerTest {
         @Test
         @DisplayName("PUT /api/v1/user - 성공")
         void updateUser() throws Exception {
-            UserDetailResponse userDetailResponse = new UserDetailResponse(1L, "John Updated", "john@example.com", "newpassword", "john123updated");
+            UserDetailResponse userDetailResponse = new UserDetailResponse(1L, "John Updated", "john@example.com", "newpassword", "john123updated", "1L");
 
             Mockito.when(userService.updateUser(anyLong(), any(UserRequest.Update.class))).thenReturn(userDetailResponse);
 
