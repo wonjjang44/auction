c902cd73f46cde37e5ea64606213ab4ec4b77af3
임채연 <dlacodus0407@gmail.com>
2024-11-01 19:11:40 +0900

    feat : Add refresh token model
    - 리프레시 토큰 모델을 생성합니다.
    - id로 refreshtoken 값을 갖습니다.
    - 유저의 id를 열로 갖습니다.

---
+++ src/main/java/com/tasksprints/auction/domain/auth/model/RefreshToken.java
@@ -0,0 +1,23 @@
+package com.tasksprints.auction.domain.auth.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+    @Id
+    private String id;
+
+    @Column
+    private Long memberId;
+}
