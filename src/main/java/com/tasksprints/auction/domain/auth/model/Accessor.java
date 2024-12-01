3eafa4619e4921cde2bfcbc8569bb0078e98efc7
임채연 <dlacodus0407@gmail.com>
2024-12-01 20:14:06 +0900

    feat : Add Permission check AOP
    - 유저의 권한을 체크하는 AOP를 추가한다
    - 엔드포인트에서, 해당 AOP를 통해 유저가 올바른 접근을 하고 있는지 확인한다
    - 올바른 접근일 경우, controller를 실행하고 아닐 경우 예외를 던진다

--- src/main/java/com/tasksprints/auction/domain/auth/model/Accessor.java
+++ src/main/java/com/tasksprints/auction/domain/auth/model/Accessor.java
@@ -1,26 +1,26 @@
 package com.tasksprints.auction.domain.auth.model;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Builder
-@Getter
-@RequiredArgsConstructor
-public class Accessor {
+public record Accessor(Long userId, Role role) {
 
     private static final Long GUEST_USER_ID = 0L;
-    private final Long userId;
-    private final Role role;
 
     public static Accessor guest() {
         return Accessor.of(GUEST_USER_ID, Role.GUEST);
     }
+
     public static Accessor user(Long userId) {
         return Accessor.of(userId, Role.USER);
     }
+
     public static Accessor admin(Long userId) {
         return Accessor.of(userId, Role.ADMIN);
+    }
+
+    public boolean isUser() {
+        return Role.USER.equals(role);
     }
 
     private static Accessor of(Long userId, Role role) {
