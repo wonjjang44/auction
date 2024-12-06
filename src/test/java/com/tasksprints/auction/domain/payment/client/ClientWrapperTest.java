01ff861541443a5f063f26254971b9ab7bb1830c
na0th <105775683+na0th@users.noreply.github.com>
2024-12-06 12:49:36 +0900

    test : ClientWrapper 테스트 수정
    수정한 내용 :
    1) Mocking을 실수로 잘못해서 성공할 수 밖에 없는 테스트를 작성함
    -> HttpResponse를 Response로 변환해서 성공적으로 반환하는지 검증하게 수정

--- src/test/java/com/tasksprints/auction/domain/payment/client/ClientWrapperTest.java
+++ src/test/java/com/tasksprints/auction/domain/payment/client/ClientWrapperTest.java
@@ -4,21 +4,28 @@ import com.tasksprints.auction.domain.payment.api.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ClientWrapperTest {
-
     @Mock
-    private HttpClientWrapper clientWrapper;
+    private HttpClient httpClient;
+    @Mock
+    private HttpResponse<String> mockHttpResponse;
+    @InjectMocks
+    private ClientWrapper clientWrapper;
 
     @BeforeEach
     void setUp() {
@@ -31,10 +38,16 @@ class ClientWrapperTest {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://example.com"))
             .build();
-        Response<String> mockResponse = Response.success(200, "test Body");
-        when(clientWrapper.send(request)).thenReturn(mockResponse);
+
+        when(mockHttpResponse.statusCode()).thenReturn(200);
+        when(mockHttpResponse.body()).thenReturn("test Body");
+
+        when(httpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class)))
+            .thenReturn(mockHttpResponse);
+
         //when
         Response<String> result = clientWrapper.send(request);
+
         //then
         assertEquals(200, result.getStatusCode());
         assertEquals("test Body", result.getBody());
