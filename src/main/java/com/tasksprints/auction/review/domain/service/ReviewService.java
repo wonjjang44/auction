aea583ce96f4e825eb5510c1409c4f2f5747f7f1
KNU-K <knukang334@gmail.com>
2024-12-14 02:21:04 +0900

    refactor(review) : review 도메인 DDD 기반으로 마이그레이션

---
+++ src/main/java/com/tasksprints/auction/review/domain/service/ReviewService.java
@@ -0,0 +1,19 @@
+package com.tasksprints.auction.review.domain.service;
+
+import com.tasksprints.auction.review.domain.dto.request.ReviewRequest;
+import com.tasksprints.auction.review.domain.dto.response.ReviewResponse;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    ReviewResponse createReview(Long userId, Long auctionId, ReviewRequest.Create review);
+
+    List<ReviewResponse> getReviewsByUserId(Long userId);
+
+    ReviewResponse getReviewByAuctionId(Long auctionId);
+
+}
+/**
+ * 수정, 삭제 불가
+ */
