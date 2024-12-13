beeb42c69982094eb2c243f5aec0e5a5464a7da2
KNU-K <knukang334@gmail.com>
2024-12-14 02:21:48 +0900

    style(all) : 스타일 및 import 최적화 진행

--- src/main/java/com/tasksprints/auction/common/config/WebConfig.java
+++ src/main/java/com/tasksprints/auction/common/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.tasksprints.auction.common.config;
 
-import com.tasksprints.auction.common.resolver.SearchConditionResolver;
-import lombok.RequiredArgsConstructor;
+import com.tasksprints.auction.auction.application.resolver.SearchConditionResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
