1aeab1c1d998a9119532506bc84ca98c56329b81
KNU-K <knukang334@gmail.com>
2024-12-14 02:18:02 +0900

    refactor : 마이그레이션으로 인해 불필요한 파일 제거

--- src/main/java/com/tasksprints/auction/domain/bid/exception/BidNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tasksprints.auction.domain.bid.exception;
-
-public class BidNotFoundException extends RuntimeException {
-    public BidNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/tasksprints/auction/domain/user/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tasksprints.auction.domain.user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
